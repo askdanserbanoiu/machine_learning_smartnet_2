b6c99db41704c4d519addff3873442f26f4d5c74
Dan Serbanoiu <32792904+askdanserbanoiu@users.noreply.github.com>
2020-01-10 10:54:20 +0200

    save progress 1_b
    save progress 1_b

--- exercise1_b.py
+++ exercise1_b.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
 import matplotlib.pyplot as plt
 import keras.layers as l
@@ -5,16 +7,14 @@ import keras.optimizers as o
 import keras.models as m
 import keras
 
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 from keras.utils import np_utils
 from keras.datasets import mnist
 from keras.models import  Sequential
+from matplotlib import pyplot as plt
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-print(X_train.shape)
 
-
-from matplotlib import pyplot as plt
 plt.imshow(X_train[0])
 
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
@@ -28,11 +28,24 @@ X_test /= 255
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
-print(Y_train[0])
-
 model = Sequential()
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+sgd = o.SGD(lr=0.01)
+
+model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train,Y_train, epochs=3, validation_data = (X_test, Y_test))
 
 
+score = model.evaluate(X_test, Y_test, verbose=0)
+
+print(score)
 
 
 
