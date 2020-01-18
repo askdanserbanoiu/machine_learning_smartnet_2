9d3de9c15159f142c1b492375e0d3fbbf01b2cd0
Georgina Paraskevopoulou <tz0rgina@yahoo.com>
2020-01-18 23:49:47 +0200

    Simpler

--- exercise1_a.py
+++ exercise1_a.py
@@ -15,54 +15,41 @@ def d_tanh(x):
     return (1-(tanh(x)**2))
 	
 def relu(x):
-    return (max(0,x))
+    return (np.maximum(0,x))
 	
 def d_relu(x):
     if(x>0): return 1
     else: return 0
 	
-x_space = np.linspace(-5, 5, 2000)
-dx_relu=x_space.copy()
+def values_of(function,x):
+    f = np.vectorize(function) 
+    y = f(x)
+    return y
 	
-sigmoid_y=[]
-Dsigmoid_y=[]
-tanh_y=[]
-Dtanh_y=[]
-relu_y=[]
-Drelu_y=[]
-
-for x in x_space:
-    sigmoid_y.append(sigmoid(x))
-    Dsigmoid_y.append(d_sigmoid(x))
-    tanh_y.append(tanh(x))
-    Dtanh_y.append(d_tanh(x))
-    relu_y.append(relu(x))
-    if(x==0):
-	    dx_relu.remove(x) #Derivative of relu is not defined at zero 
-    Drelu_y.append(d_relu(x))	
+x = np.arange(-5, 5, 0.01)
 
 fig,axs = plt.subplots(2,2,gridspec_kw={'hspace': 0.3})
 fig.suptitle('activation functions and their derivatives')
 
-axs[0,0].plot(x_space, sigmoid_y, label='sigmoid(x)')
-axs[0,0].plot(x_space, Dsigmoid_y, label="sigmoid\'(x)")
+axs[0,0].plot(x, values_of(sigmoid,x), label="sigmoid(x)")
+axs[0,0].plot(x, values_of(d_sigmoid,x), label="sigmoid\'(x)")
 axs[0,0].set_title("sigmoid")
 axs[0,0].legend(fontsize='x-small')
 
-axs[0,1].plot(x_space, tanh_y, label='tanh(x)')
-axs[0,1].plot(x_space, Dtanh_y, label="tanh\'(x)")
+axs[0,1].plot(x, values_of(tanh,x), label='tanh(x)')
+axs[0,1].plot(x, values_of(d_tanh,x), label="tanh\'(x)")
 axs[0,1].set_title("tanh")
 axs[0,1].legend(fontsize='x-small')
 
-axs[1,0].plot(x_space, relu_y)
+axs[1,0].plot(x, values_of(relu,x))
 axs[1,0].set_title("relu")
 axs[1,0].axis([-5,5,-0.1,2])
 
-axs[1,1].plot(dx_relu, Drelu_y, ls='', color='darkorange', marker='.',  markeredgewidth=0.00001, markersize=1.9)
+axs[1,1].plot(x, values_of(d_relu,x), ls='', color='darkorange', marker='.',  markeredgewidth=0.00001, markersize=1.9)
 axs[1,1].axis([-5,5,-0.1,2])
 axs[1,1].plot(0,0, 'o', color='darkorange', markerfacecolor='white')
 axs[1,1].plot(0,1, 'o', color='darkorange', markerfacecolor='white')
-axs[1,1].set_title("Derivative of relu")
-	
+axs[1,1].set_title("Derivative of relu")	
+
 _=plt.show()
 
