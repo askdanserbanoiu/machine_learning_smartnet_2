2157efb49939805b5740d09a5ee9cc0737f697a3
Dan Serbanoiu <32792904+askdanserbanoiu@users.noreply.github.com>
2020-02-28 01:43:56 +0200

    exercise3 done
    exercise3 done

--- exercise3.py
+++ exercise3.py
@@ -1,79 +1,66 @@
-import os
-import matplotlib.image as mpimg
+import os, os.path
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import keras
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Layer
+from keras.regularizers import l2
+from keras import backend 
 import numpy as np
-import cv2 as cv
-from skimage.transform import resize
 import matplotlib.pyplot as plt
+from keras import backend as K
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
 
+def exercise3():
 
-def load_images(folder):
-    images = []
-    for filename in os.listdir(folder):
-        image_dir=os.path.join(folder, filename)
-        #image_dir = mpimg.imread(os.path.join(folder, filename))
-        if image_dir is not None:
-            images.append(image_dir)
-            # load & smoothen image
-            kernel = np.ones((7,7),np.float32)/49
-            image = cv.imread(image_dir,cv.IMREAD_GRAYSCALE)
-            image = cv.filter2D(image,-1,kernel)
-            # make numpy array
-            image = np.array(image)
-            image = resize(image, (28,28))
-            # make negative
-            image = np.ones(image.shape) - image
-            plt.imshow(image, cmap="gray")
-            plt.show()
-    return images
-     
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-imgs = load_images('digits')
+    X_train = (X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32'))/255
+    X_test = (X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32'))/255
+    Y_train = np_utils.to_categorical(y_train, 10)
+    Y_test = np_utils.to_categorical(y_test, 10)
 
-print (imgs)
+    image_generator = ImageDataGenerator(
+        rotation_range=10,
+        zoom_range = 0.05, 
+        width_shift_range=0.05,
+        height_shift_range=0.05,
+        horizontal_flip=False,
+        vertical_flip=False)
 
-"""
-image, cmap="gray")
-plt.show()
+    randidx = np.random.randint(len(X_train), size=10000)
+    x_augmented = X_train[randidx].copy()
+    y_augmented = Y_train[randidx].copy()
+    x_augmented = image_generator.flow(x_augmented, np.zeros(10000), batch_size=10000, shuffle=False).next()[0]
+    X_train = np.concatenate((X_train, x_augmented))
+    Y_train = np.concatenate((Y_train, y_augmented))
 
-# Preprocess class labels
-Y_train = np_utils.to_categorical(y_train, 10)
-Y_test = np_utils.to_categorical(y_test, 10)
- 
+    X_train_train = X_train[0:62000]
+    X_validate = X_train[62000:70000]
+    Y_train_train = Y_train[0:62000]
+    Y_validate = Y_train[62000:70000]
 
-# Define model architecture
-model = Sequential()
-"""
+    model = Sequential()
+    model.add(Conv2D(25, kernel_size=(5, 5), activation='relu', input_shape=(28,28,1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(50, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='linear', W_regularizer=l2(0.01)))
 
-"""
-Because of error:
--------------------------------------------------------------------------------- 
-ValueError: Negative dimension size caused by subtracting 3 from 1 for 
-'conv2d_2/convolution' (op: 'Conv2D') with input shapes: [?,1,28,28],[3,3,28,32].
---------------------------------------------------------------------------------
- input_shape=(1,28,28),data_format='channels_first' is added
-"""
-"""
-model.add(Convolution2D(9, (3, 3), padding='same', activation='relu', input_shape=(1,28,28),data_format='channels_first'))
-model.add(MaxPooling2D(pool_size=(3,3)))
-model.add(Convolution2D(36, (3, 3), padding='same', activation='relu',  data_format='channels_first'))
-model.add(MaxPooling2D(pool_size=(3,3)))
-model.add(Convolution2D(49, (3, 3), padding='same', activation='relu',data_format='channels_first'))
-model.add(MaxPooling2D(pool_size=(3,3)))
- 
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='relu'))
- 
-# Compile model
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
- 
-# Fit model on training data
-model.fit(X_train, Y_train, 
-          batch_size=32, nb_epoch=2, verbose=1)
- 
-# Evaluate model on test data
-score = model.evaluate(X_test, Y_test, verbose=0)
-"""
+    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0, patience=1, verbose=0, mode='auto'),
+              ModelCheckpoint(filepath='exercise3.h5', monitor='val_loss', save_best_only=True)]
+
+    model.compile(loss=keras.losses.hinge, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+    model.fit(X_train_train, Y_train_train, epochs=20, verbose=1, callbacks=callbacks, validation_data=(X_validate, Y_validate))
+    score = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+exercise3()
